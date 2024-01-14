8b6849ff38ca4223c992b51143bb4ba97bc2576e
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-15 00:05:11 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,8 +1,9 @@
 // Increment all numbers in dictionary
-// Step 1
-// renamed variables
-let incNumbers = (formatComplete, ...restVariables) => {
-  for (deleteFile in formatComplete) {
+// Step 2
+// made const identifiers
+
+const  incNumbers = (formatComplete, ...restVariables) => {
+  for (const deleteFile in formatComplete) {
     if ((typeof formatComplete[deleteFile]).charAt(0).toUpperCase() === 'N') {
       formatComplete[deleteFile] = formatComplete[deleteFile] + 1;
     }
