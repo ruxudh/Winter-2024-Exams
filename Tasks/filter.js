8e72eb3dc04eab17a4f44de2c574b8516d212e8e
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-14 23:51:25 +0200

    Update filter.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,17 +1,17 @@
 // Filter array by type name
-// Step 2
-// added const identifiers
-
-const filter = (array, type) => {
-  const remove = [];
-  for (const element of array) {
-    const index = array.indexOf(element);
-    if (typeof array[index] !== type) {
-      remove.unshift(index);
-    }
-  }
-  for (const index of remove) array.splice(index, 1);
-  return array;
-};
-
+// Step 4
+//Rewrite without mutating incoming parameters 
+ 
+const filter = (array, type) => { 
+  const remove = []; 
+  const copiedArray = Object.assign(array); 
+  for (let index = 0; index < array.length; index++) { 
+    if (typeof array[index] !== type) { 
+      remove.unshift(index); 
+    } 
+  } 
+  for (const index of remove) copiedArray.splice(index, 1); 
+  return copiedArray; 
+}; 
+ 
 module.exports = filter;
