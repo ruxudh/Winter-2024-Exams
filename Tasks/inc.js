a7312c146adf98c6a66daa943689138b67ab6cfd
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-15 00:10:28 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,15 +1,17 @@
 // Increment all numbers in dictionary
-// Step 5
-// added intermediate variable 'value'
-
-const  incNumbers = (formatComplete, ...restVariables) => {
-  for (const deleteFile of Object.keys(formatComplete)) {
-    const value = formatComplete[deleteFile];
-    if ((typeof value === 'number')) {
-      formatComplete[deleteFile] = formatComplete[deleteFile] + 1;
-    }
-  }
-  return formatComplete;
-};
-
+// Step 6
+//Rewrite without mutating incoming parameters 
+//Delete redundant parameter 
+ 
+const incNumbers = (formatComplete) => { 
+  const copiedFormatComplete = Object.assign(formatComplete); 
+  for (const deleteFile of Object.keys(formatComplete)) { 
+    const value = formatComplete[deleteFile]; 
+    if (typeof value === "number") { 
+      copiedFormatComplete[deleteFile] += 1; 
+    } 
+  } 
+  return copiedFormatComplete; 
+}; 
+ 
 module.exports = incNumbers;
