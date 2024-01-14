1fc30294b7571461e659e2842e8c519aa9dbc02a
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-15 01:17:24 +0200

    Update ip.js

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,14 +1,14 @@
 // Split string by the first occurrence of separator
-// Step 4
-// Deleted unnecessary else
+// Step 5
+// made strong equal array.length !== 4
 
 'use strict';
 
-parseIp = (string) => {
+const parseIp = (string) => {
   const resultArray = [];
   if (string === '') return;
   const array = string.split('.');
-  if (array.length != 4) return;
+  if (array.length !== 4) return;
   let j = 0;
   for (const element of array) {
     resultArray[j] = parseInt(element);
