7181830dc32a2c39c62793f9ee55fd58e266cf80
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-15 00:00:23 +0200

    Update find.js

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,10 +1,10 @@
 // Find key by value
-// Step 4
-// improved returns
+// Step 5
+// changed for...in to for...of
 
 const find = (object, ...rest) => {
   const value = rest.pop(1);
-  for (const name in object) {
+  for (const name of Object.keys(object)) {
     if (object[name] === value)  {
       return name;
     }
