27f36b4ba73571efc428c4224eae67dcf68fc3f0
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-15 01:06:05 +0200

    Update invert.js

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,17 +1,14 @@
 // Reverse an array, you can't use .reverse()
-// Step 1
-// Add use strict
+// Step 2
+// deleted all redundant cod
 
 'use strict';
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
-  T.forEach((_, i) => {
+invert = (A) => {
+  T = Object.keys(A);
+  T.forEach((i) => {
     T[i] = A.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
+  });
   return T;
 };
 
