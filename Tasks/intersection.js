20e39adb7b6afc1d0c1b16ec32aa965f480ff89c
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-15 00:18:22 +0200

    Update intersection.js

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,15 +1,19 @@
-// Find an intersection of two dictionaries
-// Step 4
-// deleted redundant assign and changed if...else to if
-
-const intersection = (object1, object2) => {
-  const firstKeys = Object.keys(object1);
-  for (const attributeName of firstKeys) {
-    if (object1[attributeName] !== object2[attributeName]) {
-      delete object1[attributeName];
-    }
-  }
-  return object1;
-};
-
+// Find an intersection of two dictionaries 
+// Step 5
+// Rewrite without mutating incoming parameters 
+// Add use strict
+ 
+'use strict'; 
+ 
+const intersection = (object1, object2) => { 
+  const firstKeys = Object.keys(object1); 
+  const copiedObject = Object.assign(object1); 
+  for (const attributeName of firstKeys) { 
+    if (object1[attributeName] !== object2[attributeName]) { 
+      delete copiedObject[attributeName]; 
+    } 
+  } 
+  return copiedObject; 
+}; 
+ 
 module.exports = intersection;
