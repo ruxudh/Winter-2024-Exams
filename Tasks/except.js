91c5e990fe9b7a62b0122f9ca13ed2bf3e569a76
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-14 23:37:50 +0200

    Update except.js

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,15 +1,17 @@
 // Copy all values from dict except listed
-// Step 4
-// added const in declaration of variables and functions
-
-const except = (incomingValuesArray, ...deletingElements) => {
-  const intermediateVariable = Object.keys(incomingValuesArray);
-  intermediateVariable.forEach((element) => {
-    if (deletingElements.includes(element)) {
-      delete incomingValuesArray[element];
-    }
-  });
-  return incomingValuesArray;
-};
-
+// Step 5
+// Rewrite forEach with for of 
+// Rewrite without mutating incoming parameters 
+ 
+const except = (incomingValuesArray, ...deletingElements) => { 
+  const intermediateVariable = Object.keys(incomingValuesArray); 
+  const copiedObject = Object.assign(incomingValuesArray); 
+  for (const element of intermediateVariable) { 
+    if (deletingElements.includes(element)) { 
+      delete copiedObject[element]; 
+    } 
+  } 
+  return copiedObject; 
+}; 
+ 
 module.exports = except;
