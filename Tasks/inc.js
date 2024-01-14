e406581a4fc9040c89181494b4443657504260fa
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-15 00:26:09 +0200

    Update inc.js

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,8 +1,9 @@
 // Increment all numbers in dictionary
-// Step 6
-//Rewrite without mutating incoming parameters 
-//Delete redundant parameter 
- 
+// Step 7
+// Add use strict
+
+'use strict';
+
 const incNumbers = (formatComplete) => { 
   const copiedFormatComplete = Object.assign(formatComplete); 
   for (const deleteFile of Object.keys(formatComplete)) { 
