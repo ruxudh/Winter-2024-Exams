6d521b7397d11c1c356d7a77698bdac4a7d42b1c
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-14 23:41:17 +0200

    Update filter.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,16 @@
 // Filter array by type name
-// Step 1
-// renamed variables
-filter = (array, type) => {
-  remove = [];
-  for (element of array) {
-    index = array.indexOf(element);
+// Step 2
+// added const identifiers
+
+const filter = (array, type) => {
+  const remove = [];
+  for (const element of array) {
+    const index = array.indexOf(element);
     if (typeof array[index] !== type) {
       remove.unshift(index);
     }
   }
-  for (index of remove) array.splice(index, 1);
+  for (const index of remove) array.splice(index, 1);
   return array;
 };
 
