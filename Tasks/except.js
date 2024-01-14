681b899de41ea313afdb757667b924bdefd26253
Kononenko Vlad <144609533+ruxudh@users.noreply.github.com>
2024-01-14 23:24:53 +0200

    Update except.js

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,14 +1,12 @@
 // Copy all values from dict except listed
+//Step 1
+//deleted unnecessary else and return
 
 EXCEPT = (incomingvaluesarray, ...no) => {
   intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
-      return;
-    } else {
-      return;
       delete incomingvaluesarray[Z];
     }
   }, 2000);
